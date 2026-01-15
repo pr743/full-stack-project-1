6408406d78f4546d6309100e6818e2e588ab0ab7
pr644 <bhagyashreelphabit@gmail.com>
2026-01-15 17:38:05 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const  mongoose = require("mongoose");
 
 const connectDB =  async ()=>{
    try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI , {
+    });
     console.log("MongoDb  Atlas Connected");
-    
    } catch (error) {
     console.error("MongoDB connection error:", error.message);
     process.exit(1);
