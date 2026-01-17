15585c086f2854ee7767f68713943497597f8ef8
pr644 <bhagyashreelphabit@gmail.com>
2026-01-17 18:58:11 +0530

    first commit

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -1,39 +1,33 @@
-const axios = require("axios");
+const nodemailer = require("nodemailer");
 
 const sendOtp = async (to, otp) => {
   try {
-  if(!process.env.BREVO_API_KEY){
-    throw new Error("BREVO_API_KEY is missing");
-  }
-    await axios.post(
-      "https://api.brevo.com/v3/smtp/email",
-      {
-        sender: {
-          name: "Admin Panel",
-          email: process.env.BREVO_SENDER_EMAIL,
-        },
-        to: [{ email: to }],
-        subject: "Your OTP Code",
-        htmlContent: `
-          <h2>OTP Verification</h2>
-          <p>Your OTP is:</p>
-          <h1 style="letter-spacing:5px;">${otp}</h1>
-          <p>This OTP is valid for <b>30 seconds</b>.</p>
-        `,
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.BREVO_SMTP_LOGIN, 
+        pass: process.env.BREVO_SMTP_PASS,  
       },
-      {
-        headers: {
-          "api-key": process.env.BREVO_API_KEY,
-          "Content-Type": "application/json",
-          accept: "application/json",
-        },
-        timeout: 15000,
-      }
-    );
+    });
 
-    console.log("OTP sent via Brevo API:", to);
+    await transporter.sendMail({
+      from: `"Admin Panel" <${process.env.BREVO_SENDER_EMAIL}>`,
+      to,
+      subject: "Your OTP Code",
+      html: `
+        <h2>OTP Verification</h2>
+        <p>Your OTP is:</p>
+        <h1 style="letter-spacing:5px;">${otp}</h1>
+        <p>This OTP is valid for <b>30 seconds</b>.</p>
+      `,
+    });
+
+    console.log(" OTP sent successfully to:", to);
   } catch (err) {
-    console.log(err);
+    console.error(" SMTP OTP Error:", err.message);
+    throw err;
   }
 };
 
