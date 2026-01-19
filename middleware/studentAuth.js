c22f65aca67617e7b16f441c5395474a4ce05b75
pr644 <bhagyashreelphabit@gmail.com>
2026-01-19 19:47:25 +0530

    update page

--- middleware/studentAuth.js
+++ middleware/studentAuth.js
@@ -1,34 +1,32 @@
 const jwt = require("jsonwebtoken");
 const Student = require("../models/Student");
-const { models } = require("mongoose");
-
-
-const JWT_SECRET = process.env.JWT_SECRET;
 
 const studentAuth = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith("Bearer")) {
-      return res.status(401).json({ message: "Unauthorized" });
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "No token provided" });
     }
 
     const token = authHeader.split(" ")[1];
 
-    const decoded = jwt.verify(token, JWT_SECRET);
-
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const student = await Student.findById(decoded.id).select("-password");
 
     if (!student) {
-      return res.status(401).json({ message: "student not found" });
+      return res.status(401).json({ message: "Student not found" });
     }
 
+   
     req.studentId = student._id;
-    req.student  = student;
+    req.student = student;
+
     next();
   } catch (error) {
-    res.status(401).json({ message: "Invalid Token" });
+    console.error("Auth error:", error.message);
+    return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
 
