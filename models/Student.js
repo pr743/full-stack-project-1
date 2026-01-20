feadf6fcaacd5f1472300a97925575f129680069
pr644 <bhagyashreelphabit@gmail.com>
2026-01-20 15:07:17 +0530

    update page

--- models/Student.js
+++ models/Student.js
@@ -32,7 +32,7 @@ const studentSchema = new mongoose.Schema(
     instituteId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Institute",
-      require:true,
+      require:false,
     },
 
 
