6fe08aaa3f36b83a469738dc2399142cc10b27ac
pr644 <bhagyashreelphabit@gmail.com>
2026-01-20 16:23:21 +0530

    update page

--- controllers/studentController1.js
+++ controllers/studentController1.js
@@ -3,7 +3,7 @@ const Result = require("../models/Result");
 exports.studentDashboard = async (req, res) => {
   try {
     const student = req.student._id;
-     console.log("STUDENT ID:", req.student._id);
+     console.log("STUDENT ID:", student);
 
     const result = await Result.findOne({
       studentId: student._id,
